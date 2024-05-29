b480ae3215c00a41c0bc39898edb2b20204311fc
CristianoVieira1 <cristianovieirati@gmail.com>
2024-05-29 12:39:34 -0300

    alteracao maps

--- ios/deliveryspacex/AppDelegate.mm
+++ ios/deliveryspacex/AppDelegate.mm
@@ -8,7 +8,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  [GMSServices provideAPIKey:@"AIzaSyBCc9PsGb0nbu7BLBgDzuNYjiw7tx_lMzk"];
+  [GMSServices provideAPIKey:@""];
   self.moduleName = @"main";
 
   // You can add your custom initial props in the dictionary below.
